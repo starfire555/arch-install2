ee624c7ce509676bdb36ebedfa6c5225a2792a16
starfire555 <76488394+starfire555@users.noreply.github.com>
2023-02-14 06:51:21 +0000

    Update x05-main-software.sh

--- x05-main-software.sh
+++ x05-main-software.sh
@@ -1,13 +1,5 @@
-sudo pacman -S --noconfirm neofetch btop plocate arch-install-scripts firefox telegram-desktop community/code obsidian nomacs vlc audacity simplescreenrecorder libreoffice gimp flameshot remmina youtube-dl pavucontrol qbittorrent icecast
-#read -p "Press enter to continue"
-sudo pacman -S virt-manager qemu qemu-arch-extra ovmf vde2 ebtables dnsmasq bridge-utils openbsd-netcat
-#read -p "Press enter to continue"
-#sudo pacman -S --noconfirm cockpit
-sudo pacman -S --noconfirm --asdeps freerdp spice-gtk
-# cockpit-pcp cockpit-storaged cockpit-packagekit cockpit-podman cockpit-machines
-sudo systemctl enable icecast
-#sudo systemctl enable cockpit.socket
-sudo systemctl enable libvirtd
+sudo pacman -S --noconfirm neofetch btop plocate arch-install-scripts firefox telegram-desktop community/code obsidian nomacs vlc audacity simplescreenrecorder libreoffice gimp flameshot remmina youtube-dl pavucontrol qbittorrent
+
 
 echo ""
 echo "######################################################"
